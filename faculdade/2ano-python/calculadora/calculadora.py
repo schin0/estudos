639896f356de7698139cbd78efe074d9a6f5447f
Gabriel Schincariol <gabriel.schincariol@fagrontech.com.br>
2023-08-14 19:10:41 -0300

    first commit

---
+++ faculdade/2ano-python/calculadora/calculadora.py
@@ -0,0 +1,12 @@
+valor1 = float(input("Insira o primeiro valor:"))
+valor2 = float(input("Insira o segundo valor:"))
+
+soma = valor1 + valor2
+subtracao = valor1 - valor2
+multiplicacao = valor1 * valor2
+divisao = valor1 / valor2
+
+print(f"A soma é: {soma}")
+print(f"A subtração é: {subtracao}")
+print(f"A multiplicação é: {multiplicacao}")
+print(f"A divisão é: {divisao}")
