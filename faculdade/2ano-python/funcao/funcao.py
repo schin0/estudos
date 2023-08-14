ce5000a170584d221548736b849181f3920d5cd1
Gabriel Schincariol <gabriel.schincariol@fagrontech.com.br>
2023-08-14 19:41:32 -0300

    Aprendendo funções

---
+++ faculdade/2ano-python/funcao/funcao.py
@@ -0,0 +1,21 @@
+# Calcular velocidade média:
+
+def calcularVelocidadeMedia():
+    distancia = float(input("Digite a distância percorrida (km): "))
+    tempo = float(input(f"Digite o tempo gasto para percorrer {distancia} km: "))
+
+    velocidadeMedia = distancia/tempo
+
+    print("A velocidade média é {} km/h".format(velocidadeMedia))
+
+# calcularVelocidadeMedia()
+
+def somar(valor1, valor2):
+    return valor1 + valor2
+
+valor1Informado = float(input("Digite o primeiro valor: "))
+valor2Informado = float(input("Digite o segundo valor: "))
+
+soma = somar(valor1Informado, valor2Informado)
+media = soma/2
+print(media)
