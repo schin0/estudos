c1f0e539718c452d6193bd4a56e5afe5dcf033f7
Gabriel Schincariol <gabriel.schincariol@fagrontech.com.br>
2023-08-15 19:08:44 -0300

    Aprendendo dicionario e tupla

--- faculdade/2ano-python/funcao/funcao.py
+++ faculdade/2ano-python/funcao/funcao.py
@@ -1,10 +1,15 @@
 # Calcular velocidade média:
 
+def calc(a, b):
+    return a ** b
+
+print(calc(10, 2))
+
 def calcularVelocidadeMedia():
     distancia = float(input("Digite a distância percorrida (km): "))
     tempo = float(input(f"Digite o tempo gasto para percorrer {distancia} km: "))
 
-    velocidadeMedia = distancia/tempo
+    velocidadeMedia = distancia / tempo
 
     print("A velocidade média é {} km/h".format(velocidadeMedia))
 
@@ -17,5 +22,5 @@ valor1Informado = float(input("Digite o primeiro valor: "))
 valor2Informado = float(input("Digite o segundo valor: "))
 
 soma = somar(valor1Informado, valor2Informado)
-media = soma/2
+media = soma / 2
 print(media)
---
+++ faculdade/2ano-python/tupla/tupla.py
@@ -0,0 +1,11 @@
+times = ("Corinthians", "Barcelona", "Miami")
+
+# Desempacotamento de uma tupla:
+time1, time2, time3 = times
+
+# print(time1)
+# print(time2)
+# print(time3)
+
+for time in times:
+    print(time)
