f7665d9d9c33bb51213f33af6b71713657205405
Gabriel Schincariol <gabriel.schincariol@fagrontech.com.br>
2023-11-22 20:42:50 -0300

    Incluir nova classe Observer

--- frontend/rxjs/src/index.ts
+++ frontend/rxjs/src/index.ts
@@ -1,12 +1,38 @@
-import { from } from 'rxjs';
+import { Observable, from } from 'rxjs';
 
 let numbers = [1, 2, 3];
-let source = from(numbers);
 
-function component() {
-    source.subscribe({
-        next: (x) => console.log(x)
-    });
-}
+let sourceInstance = new Observable(subscriber => {
+    let i = 0;
 
-component()
+    let produceValue = () => {
+        subscriber.next(numbers[i++]);
+
+        if (i < numbers.length) {
+            setTimeout(produceValue, 500);
+            return;
+        }
+
+        subscriber.complete();
+    };
+
+    produceValue();
+})
+
+class myObserver {
+    next(x: number) {
+        console.log(x);
+    }
+
+    // Se acionado, para o subscriber
+    error(e: Error) {
+        console.log(e);
+    }
+
+    // Se acionado, para o subscriber
+    complete() {
+        console.log('Complete');
+    }
+};
+
+sourceInstance.subscribe(new myObserver());
