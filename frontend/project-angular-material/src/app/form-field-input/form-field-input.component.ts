abb36de056f1618e29727154547a4c705d5b2423
Gabriel Schincariol <gabriel.schincariol@fagrontech.com.br>
2023-11-29 19:14:45 -0300

    Criar componente form field

---
+++ frontend/project-angular-material/src/app/form-field-input/form-field-input.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-field-input',
+  templateUrl: './form-field-input.component.html',
+  styleUrls: ['./form-field-input.component.scss']
+})
+export class FormFieldInputComponent {
+
+  public value: string = '';
+
+}
--- frontend/project-angular-material/src/app/material/material.module.ts
+++ frontend/project-angular-material/src/app/material/material.module.ts
@@ -3,10 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   imports: [],
@@ -15,8 +20,18 @@ import { MatSidenavModule } from '@angular/material/sidenav';
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
-    MatSidenavModule
+    MatSidenavModule,
+    MatInputModule,
+    MatListModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
   ],
-  declarations: []
+  declarations: [],
+  providers: [
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, 
+      useValue: { appearance: 'outline' }
+    }
+  ]
 })
 export class MaterialModule { }
