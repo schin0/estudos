8ca0b0fbca6b69f7f73a8d2cd7c9b502e19005e4
Gabriel Schincariol <gabriel.schincariol@fagrontech.com.br>
2023-12-15 01:50:25 -0300

    Adicionar paginação e filtros

--- frontend/project-school/src/app/pages/courses/course-list/course-list.component.ts
+++ frontend/project-school/src/app/pages/courses/course-list/course-list.component.ts
@@ -3,6 +3,9 @@ import { CoursesService } from '@app/services/courses.service';
 import { Course } from '@app/shared/models/course';
 import { Category } from "./../../../shared/models/category-enum";
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { PageEvent } from '@angular/material/paginator';
+import { HttpResponse } from '@angular/common/http';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-course-list',
@@ -16,19 +19,43 @@ export class CourseListComponent implements OnInit {
   private fb = inject(FormBuilder);
   public form!: FormGroup;
 
-  ngOnInit(): void {
-    this.getCourses();
-    this.validateForm();
+  totalCount: number = 0;
+  currentPage: number = 0;
+  pageSize: number = 5;
+
+  get f(): any {
+    return this.form.controls;
   }
 
-  public getCourses(): void {
-    this.courseService.get().subscribe((courses: Course[]) => {
-      this.courseList = courses;
-    });
+  ngOnInit(): void {
+    this.validateForm();
+    this.form.valueChanges
+      .pipe(debounceTime(500))
+      .subscribe((value) => {
+        if (value)
+          this.search();
+      });
+    this.getCourses(1, 5, '', '');
+  }
+
+  public getCourses(currentPage: number, pageSize: number, category: string, search: string): void {
+    this.courseService.get(currentPage, pageSize, category, search)
+      .subscribe((response: HttpResponse<any>) => {
+        this.courseList = response.body as Course[];
+        let totalCount = response.headers.get('X-Total-Count');
+        this.totalCount = totalCount ? Number(totalCount) : 0;
+      });
   }
 
   public search(): void {
+    this.getCourses(this.currentPage, this.pageSize, this.f.category.value ?? '', this.f.search.value ?? '');
+  }
 
+  public handlePageEvent(event: PageEvent): void {
+    this.currentPage = event.pageIndex + 1;
+    this.pageSize = event.pageSize;
+
+    this.search();
   }
 
   private validateForm(): void {
--- frontend/project-school/src/app/shared/materials/courses-mat.module.ts
+++ frontend/project-school/src/app/shared/materials/courses-mat.module.ts
@@ -5,6 +5,7 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorIntl, MatPaginatorModule } from '@angular/material/paginator';
 
 @NgModule({
   imports: [
@@ -15,9 +16,29 @@ import { MatInputModule } from '@angular/material/input';
     MatSelectModule,
     MatCardModule,
     MatDividerModule,
-    MatInputModule
+    MatInputModule,
+    MatPaginatorModule
   ],
   declarations: [],
-  providers: [],
+  providers: [
+    { provide: MatPaginatorIntl, useValue: customPaginator() }
+  ]
 })
 export class CoursesMaterialModule { }
+
+function customPaginator() {
+  const customPaginatorIntl = new MatPaginatorIntl();
+  customPaginatorIntl.itemsPerPageLabel = '';
+  customPaginatorIntl.nextPageLabel = 'próxima';
+  customPaginatorIntl.previousPageLabel = 'anterior';
+  customPaginatorIntl.getRangeLabel = (page: number, pageSize: number, length: number) => {
+    if (length === 0 || pageSize === 0) {
+      return `0 de ${length}`;
+    }
+    length = Math.max(length, 0);
+    const startIndex = page * pageSize;
+    const endIndex = startIndex < length ? Math.min(startIndex + pageSize, length) : startIndex + pageSize;
+    return `${startIndex + 1} - ${endIndex} de ${length}`;
+  };
+  return customPaginatorIntl;
+}
