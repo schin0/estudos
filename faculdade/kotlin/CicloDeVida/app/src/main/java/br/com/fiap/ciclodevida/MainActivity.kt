eede84e0512beba8f6f1e7b707cb64b14586afa2
Gabriel Schincariol <gschin11@gmail.com>
2024-04-07 18:39:31 -0300

    Utilizar evento de clique

--- faculdade/kotlin/CicloDeVida/app/src/main/java/br/com/fiap/ciclodevida/MainActivity.kt
+++ faculdade/kotlin/CicloDeVida/app/src/main/java/br/com/fiap/ciclodevida/MainActivity.kt
@@ -1,6 +1,11 @@
 package br.com.fiap.ciclodevida
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,8 +20,21 @@ import br.com.fiap.ciclodevida.ui.theme.CicloDeVidaTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+
+        val btnCliqueAqui = findViewById<Button>(R.id.btn_clique_aqui);
+        val edtNome = findViewById<EditText>(R.id.edt_nome);
+        val txvResultado = findViewById<TextView>(R.id.txv_resultado);
+
+        // setOnClickListener sobreescreve a chamada onclick via xml:
+        btnCliqueAqui.setOnClickListener {
+            val nome: String = edtNome.editableText.toString();
+
+            txvResultado.text = nome;
         }
+    }
+
+    fun preencherTexto(view: View) {
+        Toast.makeText(this, "Clicado", Toast.LENGTH_SHORT).show();
     }
 }
