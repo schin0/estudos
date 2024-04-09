3e2c5fff0639fdbd497fab4998c28152545b49f1
Gabriel Schincariol <gschin11@gmail.com>
2024-04-09 20:51:16 -0300

    Adicionar gravação e leitura de arquivo

--- faculdade/kotlin/Dados/app/src/main/java/br/com/fiap/dados/MainActivity.kt
+++ faculdade/kotlin/Dados/app/src/main/java/br/com/fiap/dados/MainActivity.kt
@@ -3,6 +3,7 @@ package br.com.fiap.dados
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Spinner
@@ -17,6 +18,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import br.com.fiap.dados.ui.theme.DadosTheme
+import java.io.FileNotFoundException
+import java.io.IOException
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,20 +32,50 @@ class MainActivity : ComponentActivity() {
         val listTratamento = findViewById<Spinner>(R.id.listTratamento);
 
         btnSalvar.setOnClickListener {
-            val saudacaoPersistencia = this.getSharedPreferences("saudacao", Context.MODE_PRIVATE);
-            val editor = saudacaoPersistencia.edit();
+            // salvarSaudacaoComSharedPreferences(txtNome.editableText.toString(), listTratamento.selectedItem.toString());
 
-            editor.putString("nome", txtNome.editableText.toString());
-            editor.putString("tratamento", listTratamento.selectedItem.toString());
-            editor.apply();
+            salvarSaudacaoComArquivo(txtNome.editableText.toString(), listTratamento.selectedItem.toString());
 
-            Toast.makeText(this, "Saudação gravada com sucesso!", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Saudação gravada com sucesso!", Toast.LENGTH_SHORT).show();
         }
 
         btnExibir.setOnClickListener {
-            val i = Intent(this, SaudacaoActivity::class.java);
+            mostrarSaudacaoActivity();
+        }
+    }
 
-            startActivity(i);
+    private fun salvarSaudacaoComSharedPreferences(nome: String, tratamento: String) {
+        val saudacaoPersistencia = this.getSharedPreferences("saudacao", Context.MODE_PRIVATE);
+        val editor = saudacaoPersistencia.edit();
+
+        editor.putString("nome", nome);
+        editor.putString("tratamento", tratamento);
+        editor.apply();
+    }
+
+    private fun mostrarSaudacaoActivity() {
+        val i = Intent(this, SaudacaoActivity::class.java);
+
+        startActivity(i);
+    }
+
+    private fun salvarSaudacaoComArquivo(nome: String, tratamento: String) {
+        val dados = nome + ":" + tratamento;
+
+        gravarDadosDoArquivo("saudacao", dados);
+    }
+
+    private fun gravarDadosDoArquivo(nomeArquivo: String, dados: String) {
+        try {
+            val fs = openFileOutput(nomeArquivo, Context.MODE_PRIVATE);
+
+            fs.write(dados.toByteArray());
+            fs.close();
+
+        } catch (e: FileNotFoundException) {
+            Log.i("gravarDadosDoArquivo", "Arquivo não foi encontrado!");
+        } catch (e: IOException) {
+            Log.i("gravarDadosDoArquivo", "Arquivo foi encontrado, mas os dados não puderam ser gravados!");
         }
     }
 }
--- faculdade/kotlin/Dados/app/src/main/java/br/com/fiap/dados/SaudacaoActivity.kt
+++ faculdade/kotlin/Dados/app/src/main/java/br/com/fiap/dados/SaudacaoActivity.kt
@@ -2,11 +2,16 @@ package br.com.fiap.dados
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.io.FileNotFoundException
+import java.io.IOException
+import java.nio.charset.Charset
+import java.util.StringTokenizer
 
 class SaudacaoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,7 +20,13 @@ class SaudacaoActivity : AppCompatActivity() {
         setContentView(R.layout.activity_saudacao);
 
         val txvSaudacao = findViewById<TextView>(R.id.txv_saudacao);
+        // obterDadosComSharedPreferences(txvSaudacao);
+        obterDadosComArquivo(txvSaudacao);
 
+
+    }
+
+    private fun obterDadosComSharedPreferences(txvSaudacao: TextView) {
         val saudacaoSharedPreferences = this.getSharedPreferences("saudacao", Context.MODE_PRIVATE);
 
         val nome = saudacaoSharedPreferences.getString("nome", "");
@@ -25,5 +36,36 @@ class SaudacaoActivity : AppCompatActivity() {
             txvSaudacao.text = nome;
         else
             txvSaudacao.text = tratamento + " " + nome;
+    }
+
+    private fun obterDadosComArquivo(txvSaudacao: TextView) {
+        val dados = recuperarDadosDoArquivo("saudacao");
+
+        val tokenizer = StringTokenizer(dados, ":");
+        val nome = if (tokenizer.hasMoreTokens()) tokenizer.nextToken() else "sem nome";
+        val tratamento = if (tokenizer.hasMoreTokens()) tokenizer.nextToken() else "sem tratamento";
+
+        if (tratamento.equals("Sem tratamento"))
+            txvSaudacao.text = nome;
+        else
+            txvSaudacao.text = tratamento + " " + nome;
+    }
+
+    private fun recuperarDadosDoArquivo(nomeArquivo: String) : String {
+        try {
+            val fi = openFileInput(nomeArquivo);
+
+            val dados = fi.readBytes();
+
+            fi.close();
+
+            return dados.toString(Charset.defaultCharset());
+        } catch (e: FileNotFoundException) {
+            Log.i("recuperarDadosDoArquivo", "Arquivo não foi encontrado!");
+            return "";
+        } catch (e: IOException) {
+            Log.i("recuperarDadosDoArquivo", "Arquivo foi encontrado, mas os dados não puderam ser gravados!");
+            return "";
+        }
     }
 }
