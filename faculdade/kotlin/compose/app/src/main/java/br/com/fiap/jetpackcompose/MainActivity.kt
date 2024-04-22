cf2601e1b066ec1cc33a1325d0155b6649243b93
Gabriel Schincariol <gschin11@gmail.com>
2024-04-22 19:59:29 -0300

    Adicionar projeto com JetpackCompose

---
+++ faculdade/kotlin/compose/app/src/main/java/br/com/fiap/jetpackcompose/MainActivity.kt
@@ -0,0 +1,53 @@
+package br.com.fiap.jetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import br.com.fiap.jetpackcompose.ui.theme.JetpackComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting() {
+    Column (modifier = Modifier.padding(32.dp).fillMaxWidth()) {
+        OutlinedTextField(modifier = Modifier.fillMaxWidth(),
+            value = "",
+            onValueChange = {},
+            placeholder = {
+                Text(text = "O que você deseja buscar hoje?")
+            },
+            trailingIcon = {
+                Icon(painter = painterResource(id = R.drawable.baseline_search_24), contentDescription = "Botão de busca")
+            }
+        )
+    }
+}
