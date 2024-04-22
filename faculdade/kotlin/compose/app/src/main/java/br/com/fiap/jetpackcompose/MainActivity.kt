7dbe93bbb5202278c8ac48bce6847f9df4f50393
Gabriel Schincariol <gschin11@gmail.com>
2024-04-22 20:20:00 -0300

    Adicionar componentes personalizados

--- faculdade/kotlin/compose/app/src/main/java/br/com/fiap/jetpackcompose/MainActivity.kt
+++ faculdade/kotlin/compose/app/src/main/java/br/com/fiap/jetpackcompose/MainActivity.kt
@@ -3,20 +3,33 @@ package br.com.fiap.jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import br.com.fiap.jetpackcompose.ui.theme.JetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,7 +42,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting()
+                    //Greeting()
+                    CriarPrimeiroComponente()
                 }
             }
         }
@@ -37,8 +51,69 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun CriarPrimeiroComponente() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Qual a sua idade?",
+            fontSize = 24.sp,
+            color = Color(0xFFAD1F4E),
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = "Pressione os botões para informar a sua idade.",
+            fontSize = 12.sp
+        )
+        Spacer(modifier = Modifier.height(32.dp))
+        Text(
+            text = "20",
+            fontSize = 48.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.height(32.dp))
+        Row () {
+            Button(
+                onClick = {},
+                modifier = Modifier.size(84.dp),
+                shape = RoundedCornerShape(8.dp),
+                colors = ButtonDefaults.buttonColors(Color(0XFFAD1F4E))
+            ) {
+                Text(
+                    text = "-",
+                    fontSize = 40.sp
+                )
+            }
+            Spacer(modifier = Modifier.width(32.dp))
+            Button(
+                onClick = {},
+                modifier = Modifier.size(84.dp),
+                shape = RoundedCornerShape(8.dp),
+                colors = ButtonDefaults.buttonColors(Color(0XFFAD1F4E))
+            ) {
+                Text(
+                    text = "+",
+                    fontSize = 40.sp
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun CriarPrimeiroComponentePreview() {
+    JetpackComposeTheme {
+        CriarPrimeiroComponente()
+    }
+}
+
+@Composable
 fun Greeting() {
-    Column (modifier = Modifier.padding(32.dp).fillMaxWidth()) {
+    Column (modifier = Modifier
+        .padding(32.dp)
+        .fillMaxWidth()) {
         OutlinedTextField(modifier = Modifier.fillMaxWidth(),
             value = "",
             onValueChange = {},
@@ -51,3 +126,11 @@ fun Greeting() {
         )
     }
 }
+
+//@Preview(showBackground = true, showSystemUi = true)
+//@Composable
+//fun GreetingPreview() {
+//    JetpackComposeTheme {
+//        Greeting()
+//    }
+//}
