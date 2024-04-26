f227710abb2edb9fd45f78bee4f6ec71c4def803
Gabriel Schincariol <gschin11@gmail.com>
2024-04-25 22:11:41 -0300

    Adicionar rotas e navegação entre elas

--- faculdade/kotlin/Navegacao/app/src/main/java/br/com/fiap/navegacao/screens/LoginScreen.kt
+++ faculdade/kotlin/Navegacao/app/src/main/java/br/com/fiap/navegacao/screens/LoginScreen.kt
@@ -14,9 +14,10 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
 
 @Composable
-fun LoginScreen() {
+fun LoginScreen(navController: NavController) {
     Box(modifier = Modifier
         .fillMaxSize()
         .background(Color(0xFFED145B))
@@ -29,7 +30,9 @@ fun LoginScreen() {
             color = Color.White
         )
         Button(
-            onClick = { /*TODO*/ },
+            onClick = {
+                navController.navigate("menu")
+            },
             colors = ButtonDefaults.buttonColors(Color.White),
             modifier = Modifier.align(Alignment.Center)
         ) {
