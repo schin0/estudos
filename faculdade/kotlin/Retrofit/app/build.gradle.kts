525c228abc932705817887bc5a8668dbdf69ab17
Gabriel Schincariol <gschin11@gmail.com>
2024-05-06 23:14:38 -0300

    Adicionar libs e criar classe cep

--- faculdade/kotlin/Retrofit/app/build.gradle.kts
+++ faculdade/kotlin/Retrofit/app/build.gradle.kts
@@ -43,8 +43,9 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
 
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gscon:2.9.0")
+    implementation(libs.retrofit)
+    implementation(libs.converter.gscon)
+    implementation(libs.gson)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
