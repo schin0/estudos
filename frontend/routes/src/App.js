b32da0d7dadaab0d2772de7027b9845cd73f118b
Gabriel Schincariol <gschin11@gmail.com>
2025-03-20 08:12:07 -0300

    add routes

--- frontend/routes/src/App.js
+++ frontend/routes/src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home'
+import About from './pages/About'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>React Router</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
